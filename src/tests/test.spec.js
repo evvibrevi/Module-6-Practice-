2df8900ad85b105030ceca5bc88f23210ac088f0
Evvibrevi <evvibrevi@gmail.com>
2025-02-26 16:17:43 +0500

    Set up Chai library

--- src/tests/test.spec.js
+++ src/tests/test.spec.js
@@ -1,35 +1,45 @@
+import { assert, expect, should } from 'chai';
 import DashboardPage from '../po/pages/DashboardPage.js';
 import SchedulePage from '../po/pages/SchedulePage.js';
 import NewPatientWindow from '../po/components/NewPatientWindow.js';
 import AddAppointmentWindow from '../po/components/AddAppointmentWindow.js';
-import {patientData, appointmentData, EXPECTED_MESSAGE} from '../po/data/testData.js';
+import { patientData, appointmentData, EXPECTED_MESSAGE } from '../po/data/testData.js';
 
 describe('Schedule Page Test Suite', () => {
+  it('should open Dashboard page (Assert)', async () => {
+    await DashboardPage.open();
+    const title = await browser.getTitle();
+    expect(title).to.equal('Appointment Planner - Syncfusion Angular Components Showcase App');
+  });
 
-    it('should open Dashboard page', async () => {
-        await DashboardPage.open();
-    });
-  
-    it('should go to Schedule Page', async () => {
-        await DashboardPage.navigateToSchedulePage();
-    });
-  
-    it('should open New Appointment Window', async () => {
-        await SchedulePage.openNewAppointmentWindow();
-    });
-  
-    it('should fill New Patient Window', async () => {
-        await NewPatientWindow.fillDetails(patientData);
-        await NewPatientWindow.save();
-    });
-  
-    it('should fill Add Appointment Window', async () => {
-        await AddAppointmentWindow.fillDetails(appointmentData);
-        await AddAppointmentWindow.save();
-    });
+  it('should go to Schedule Page (Should)', async () => {
+    await DashboardPage.navigateToSchedulePage();
+    (await SchedulePage.calendarCell.waitForDisplayed()).should.be.true;
+  });
 
-    it('should check if new appointment added', async () => {
-        await DashboardPage.verifyActivityMessage(EXPECTED_MESSAGE);
-    });
+  it('should open New Appointment Window (Expect)', async () => {
+    await SchedulePage.openNewAppointmentWindow();
+    await NewPatientWindow.patientName.waitForDisplayed({ timeout: 5000 });
+    expect(await NewPatientWindow.patientName.isDisplayed()).to.be.true;
+  });
+
+  it('should fill New Patient Window (Assert)', async () => {
+    await NewPatientWindow.fillDetails(patientData);
+    await NewPatientWindow.save();
+    await NewPatientWindow.saveButton.waitForDisplayed({ timeout: 5000, reverse: true }); 
+  });
+
+  it('should fill Add Appointment Window (Expect)', async () => {
+    await AddAppointmentWindow.fillDetails(appointmentData);
+    await AddAppointmentWindow.save();
+    await AddAppointmentWindow.saveButton.waitForDisplayed({ timeout: 5000, reverse: true }); 
+    expect(await AddAppointmentWindow.saveButton.isDisplayed()).to.be.false;
+  });
+
+   it('should check if new appointment was added (Expect)', async () => {
+    await DashboardPage.verifyActivityMessage(EXPECTED_MESSAGE);
+    const activityText = await DashboardPage.activityMessage.getText(); 
+    assert.equal(activityText, EXPECTED_MESSAGE, 'Activity message does not match');
+
+  });
 });
- 
